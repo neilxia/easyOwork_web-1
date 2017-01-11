f8688cb6745094772498f9b1c099b2a4b370f90f
LUOCHANGQING <luochangqing@outlook.com>
2017-01-11 17:04:35 +0800

    修改接口访问使用https://www.qinghuiyang.com

--- easyOwork_web/market/js/config.js
+++ easyOwork_web/market/js/config.js
@@ -4,7 +4,7 @@
 var config = angular.module('market.config',[]);
 config.factory('AppConfig',function(){
     return {
-        BASE_URL:'http://120.76.96.199:8080/',
+        BASE_URL:'https://www.qinghuiyang.com/',
     }
 });
 config.factory('LocalStorage',['$window',function($window){
