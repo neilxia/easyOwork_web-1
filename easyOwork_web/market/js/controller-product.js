bf66d73770d31a783ab45efe5f75084f3e115407
LUOCHANGQING <luochangqing@outlook.com>
2017-03-10 19:32:59 +0800

    修改

--- easyOwork_web/market/js/controller-product.js
+++ easyOwork_web/market/js/controller-product.js
@@ -2,7 +2,13 @@
  * Created by Dumin on 2016/7/21.
  */
 var app = angular.module('market.product',[]);
-app.controller('productCtrl',['$rootScope',function($rootScope){
+app.controller('productCtrl',['$rootScope','$scope',function($rootScope,$scope){
 	
+	
+	$scope.viewFunction = function(functionId){
+		$("#"+$scope.currentFunction).fadeOut();
+		$("#"+functionId).fadeIn();
+		$scope.currentFunction = functionId;
+	}
 	
 }]);
